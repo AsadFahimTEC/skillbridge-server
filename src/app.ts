3e1d0d1da95a1c406d570eb417cfde7a701749f6
AsadFahimTEC <fah485434@gmail.com>
2026-01-27 20:03:02 +0600

    better auth authentication started

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express, { type Application } from "express";
 
+import { toNodeHandler } from "better-auth/node";
+
+import {auth} from "../src/lib/auth";
+
 const app: Application = express();
 
+app.all("/api/auth/splat", toNodeHandler(auth))
+
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    res.send("SkillBridge is Running!")
 })
 
 export default app;
