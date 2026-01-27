0d1a5da2b45b4f468ff0c8b03121e241ed1d6753
AsadFahimTEC <fah485434@gmail.com>
2026-01-27 19:29:10 +0600

    postgress database connection added

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
-
+import {prisma} from "../src/lib/prisma";
 
 import app from "./app";
 
 async function main () {
     try {
         await prisma.$connect();
+        console.log("Connected to the database successfully.");
     } catch (error) {
-        
+        console.error("An error occurred:", error);
+        await prisma.$disconnect();
+        process.exit(1);
     }
 }
+
+main();
 
 const PORT = process.env.PORT || 5000;
 
