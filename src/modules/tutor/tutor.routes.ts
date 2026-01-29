ad39a6995d6f19de4e40aa34a84ea741613c2223
AsadFahimTEC <fah485434@gmail.com>
2026-01-29 13:56:52 +0600

    prisma all files separated properly

--- src/modules/tutor/tutor.routes.ts
+++ src/modules/tutor/tutor.routes.ts
@@ -6,7 +6,6 @@ const router = express.Router();
 
 router.get("/", tutorController.listTutors);
 
-router.get("/:id", tutorController.listTutors);
+router.get("/:id", tutorController.tutorDetails);
 
 export const tutorRouter: Router = router;
-
